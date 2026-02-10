c62a078e6922fbd39879f3ea01186d2a82da59e6
justine <justinemico42@gmail.com>
2026-02-10 10:28:47 +0800

    row and colums

---
+++ app/pages/row.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-card-text>
+        <v-row align="center">
+            <v-col>
+                <v-icon size="80">mdi-account</v-icon>
+            </v-col>
+
+            <v-col>
+                <v-row no-gutters>
+                    <v-col cols="12" class="text-right text-h6 text-uppercase">
+                    </v-col>
+                </v-row>
+            </v-col>
+
+            <v-col cols="12" class="text-right text-h3 font-weight-bold">
+                10
+            </v-col>
+
+        </v-row>
+
+    </v-card-text>
+
+
+
+
+        
+      <v-col cols="12" md="3">
+        <v-card height="60" color="yellow"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="blue"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="60" color="purple"></v-card>
+      </v-col>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
